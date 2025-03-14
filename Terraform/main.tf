3b08b06492210da768bf775dc48ac0bb7eebcc5e
soundariya sundar <154726977+soundariyasundar@users.noreply.github.com>
2025-03-14 09:47:10 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -10,8 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
 }
 
 # create security group for the ec2 instance
